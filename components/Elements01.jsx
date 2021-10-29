d5e890287a96c1deb0e26ca24acf3570910486a9
masato <aoki.masato.lmasato@gmail.com>
2021-10-29 18:48:29 +0900

    M_03

--- components/Elements01.jsx
+++ components/Elements01.jsx
@@ -1,11 +1,38 @@
 import React from 'react';
 import { StyleSheet, Text, View, } from 'react-native';
-
+import { Button,Header,Switch } from 'react-native-elements';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 const Elements01 = () => {
     return (
         <View style={styles.TitleContainer}>
+            <Header
+             leftComponent={{ icon: 'menu', color: '#fff', iconStyle: { color: '#fff' } }}
+             centerComponent={{ text: 'MY TITLE', style: { color: '#fff' } }}
+             rightComponent={{ icon: 'home', color: '#fff' }}
+            />
             <Text style={styles.Titletxt}>AokiTXT</Text>
+            <Button
+                title="Aoki"
+                type="outline"
+            />
+
+            <Button
+            icon={
+                     <Icon
+                         name="arrow-right"
+                         size={15}
+                         color="white"
+                     />
+                     }
+             title="Masato"
+            />
+
+<Switch value={true} />
+
+<Switch value={false} color="orange" />
+
+
         </View>
 
     )
