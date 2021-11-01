41e30a87b347630547a2dc15bd41490fdeba98f0
masato <aoki.masato.lmasato@gmail.com>
2021-11-01 20:59:34 +0900

    M04

---
+++ components/Elements02.jsx
@@ -0,0 +1,26 @@
+import _default from 'atob';
+import React from 'react';
+import { StyleSheet, Text, View,SafeAreaView } from 'react-native';
+import { Button,Header,Switch } from 'react-native-elements';
+import Icon from 'react-native-vector-icons/FontAwesome';
+
+const Elements02 =() => {
+    return(
+ <SafeAreaView style={styles.ItiContainer}>
+     <Text>aokioakifiji</Text>
+ </SafeAreaView>
+
+    )
+}
+
+
+const styles = StyleSheet.create({
+
+    ItiContainer:{
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+
+})
+
+export default Elements02;
